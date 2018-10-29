a80b83097f9637ad1bd0e9dd038d19c3f2692ddc
Unmesh <unmesh@Unmeshs-MacBook-Pro.local>
2018-10-29 15:09:49 +0530

    View will appear added
    View will appear added

--- hello_world/hello_world/ViewController.m
+++ hello_world/hello_world/ViewController.m
@@ -19,6 +19,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
